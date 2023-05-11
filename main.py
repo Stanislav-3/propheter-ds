09b29fe3510fb95efe1ebf7107f5d0dc3b4655cf
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2023-05-11 17:40:48 +0300

    add basic routing

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 from flask import Flask
+from api.helloworld import hello_world
 
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+app.add_url_rule('/', view_func=hello_world, methods=['GET'])
 
 
 if __name__ == '__main__':
