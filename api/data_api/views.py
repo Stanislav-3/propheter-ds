9e4c6d9adc6a9d7aff7a667e796f31e378c0a86c
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2023-06-18 13:09:21 +0300

    improve /data-api/ticks endpoint

--- api/data_api/views.py
+++ api/data_api/views.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Request, Form, Depends
+from fastapi import APIRouter, Request, Form, Depends, HTTPException, status
 from sqlalchemy.orm.session import Session
 from pydantic import BaseModel
 from datetime import datetime
@@ -10,18 +10,45 @@ from models.models_ import Stock, Kline
 data_api_router = APIRouter(prefix='/data-api')
 
 
+def parse_datetime(time: str) -> datetime:
+    parse_str = '%Y-%m-%d %H:%M:%S.%f'
+
+    try:
+        date = datetime.strptime(time, parse_str)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail=f'Time {time} cannot be parsed with {parse_str}')
+
+    return date
+
+
+def parse_float(num: str) -> float:
+    try:
+        num = float(num)
+    except ValueError:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail=f'Expected float, but get {num}')
+
+    return num
+
+
+def get_pair_id(pair_name: str, db: Session) -> int:
+    pair = db.query(Stock).filter(Stock.name == pair_name).first()
+    if pair is None:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=f'Pair {pair_name} is not found in db')
+
+    return pair.id
+
+
 @data_api_router.post("/ticks")
 async def new_ticks(pair: str = Form(...), time: str = Form(...), open: str = Form(...), close: str = Form(...),
                     high: str = Form(...), low: str = Form(...), vol: str = Form(...),
                     db: Session = Depends(get_db)):
-    print(time)
-    pair = db.query(Stock).filter(Stock.name == pair).first()
-    if pair is None:
-        pass
+    print(type(get_pair_id(pair, db)))
+    kline = Kline(stock_id=get_pair_id(pair, db), date=parse_datetime(time), low=parse_float(low),
+                  high=parse_float(high), open=parse_float(open), close=parse_float(close), volume=parse_float(vol))
 
-    db.add(
-        Kline(stock_id=pair.id, date=datetime.strptime(time, "%Y-%m-%d %H:%M:%S.%f"), low=float(low), high=float(high),
-              open=float(open), close=float(close), volume=float(vol))
-    )
+    db.add(kline)
     db.commit()
-    return {'time': time}
+    return {'message': 'Successfully added new_tick'}
