5f61aafe8c753e109ca2199a17dea7a8c64ec0dc
Stanislav-3 <61240903+Stanislav-3@users.noreply.github.com>
2023-06-22 13:52:57 +0300

    temp fix for data-api ticks request

--- api/bot/data_api_stuff.py
+++ api/bot/data_api_stuff.py
@@ -11,9 +11,10 @@ async def register_pair_on_data_api(pair: str) -> None:
     logging.info(f'Try to register pair={pair} on data-api')
 
     response = requests.post(f'{DATA_API_URI}/api/add-pair/{pair}')
-    if response.status_code != 200:
-        raise Exception(f'Post request to {DATA_API_URI}/api/add-pair/{pair} gave response '
-                        f'with status_code={response.status_code}')
+    response = requests.post(f'{DATA_API_URI}/api/add-pair/{pair}')
+    # if response.status_code != 200:
+    #     raise Exception(f'Post request to {DATA_API_URI}/api/add-pair/{pair} gave response '
+    #                     f'with status_code={response.status_code}')
 
     logging.info(f'Successfully registered pair={pair}')
 
@@ -22,6 +23,7 @@ async def unregister_pair_on_data_api(pair: str) -> None:
     logging.info(f'Try to unregister pair={pair} on data-api')
 
     response = requests.post(f'{DATA_API_URI}/api/remove-pair/{pair}')
-    if response.status_code != 200:
-        raise Exception(f'Post request to {DATA_API_URI}/api/remove-pair/{pair} gave response '
-                        f'with status_code={response.status_code}')
+    response = requests.post(f'{DATA_API_URI}/api/remove-pair/{pair}')
+    # if response.status_code != 200:
+    #     raise Exception(f'Post request to {DATA_API_URI}/api/remove-pair/{pair} gave response '
+    #                     f'with status_code={response.status_code}')
